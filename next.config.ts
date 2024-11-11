94d2fa45138392b1acc6bdbff6af4b482380c3d6
MK <qscvdefb@gmail.com>
2024-11-11 18:41:08 +0700

    reload config

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '', // Leave empty for default port
-        pathname: '/**', // Allow all paths
-      },
-    ],
-  },
-};
+
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
